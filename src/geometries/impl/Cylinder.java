7cfd8f7d766135d1dd2d760c5981c03b4666d5cf
BEN NAKACHE <nakacheben5@gmail.com>
2026-03-12 22:25:30 +0200

    Class Cylinder

--- src/geometries/impl/Cylinder.java
+++ src/geometries/impl/Cylinder.java
@@ -1,4 +1,12 @@
 package geometries.impl;
 
-public class Cylinder {
+import primitives.Ray;
+
+public class Cylinder extends Tube {
+    private final double _height;
+
+    public Cylinder(Ray _axis, double _radius, double _height) {
+        super(_axis, _radius);
+        this._height = _height;
+    }
 }
