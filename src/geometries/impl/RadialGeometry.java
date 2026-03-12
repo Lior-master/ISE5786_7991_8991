7242825c6c3754d22c8504d41cbfafdb912bf434
Lior h <lior.halimi@yahoo.fr>
2026-03-12 21:58:03 +0200

    RadialGeometry class created with javadoc

---
+++ src/geometries/impl/RadialGeometry.java
@@ -0,0 +1,40 @@
+package geometries.impl;
+
+import geometries.api.Geometry;
+import primitives.Point;
+import primitives.Vector;
+
+/**
+ * Abstract base class for all radial (round) geometries in 3D space,
+ * such as spheres, cylinders, and tubes.
+ * Stores the radius and its squared value for optimized calculations.
+ *
+ * @author Halimi Lior
+ * @author Nakache Ben
+ */
+public class RadialGeometry extends Geometry {
+    /**
+     * The radius of the radial geometry.
+     */
+    protected final double _radius;
+
+    /**
+     * The squared radius, stored for performance optimization.
+     */
+    protected final double _radiusSquared;
+
+    /**
+     * Constructs a RadialGeometry with the given radius.
+     *
+     * @param radius the radius of the geometry (must be positive)
+     */
+    RadialGeometry(double radius) {
+        _radius = radius;
+        _radiusSquared = radius * radius;
+    }
+
+    @Override
+    public Vector getNormal(Point point) {
+        return null;
+    }
+}
