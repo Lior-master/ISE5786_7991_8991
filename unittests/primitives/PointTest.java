482b48fc617c53ca1290cee642db401f395c0551
Lior h <lior.halimi@yahoo.fr>
2026-03-18 16:16:48 +0200

    adding PointTest class to unittests

---
+++ unittests/primitives/PointTest.java
@@ -0,0 +1,32 @@
+package primitives;
+
+class PointTest {
+
+    @org.junit.jupiter.api.Test
+    void subtract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void distanceSquared() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void distance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testHashCode() {
+    }
+}
