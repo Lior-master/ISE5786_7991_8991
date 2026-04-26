499f6809bac778f6b0327a65307b518a7509524d
Lior h <lior.halimi@yahoo.fr>
2026-04-26 15:56:55 +0300

    adding javadoc to Geometries

--- src/geometries/impl/Geometries.java
+++ src/geometries/impl/Geometries.java
@@ -9,13 +9,35 @@ import primitives.Ray;
 
 import static java.util.Collections.addAll;
 
+/**
+ * Represents a composite geometry that groups multiple {@link Intersectable}
+ * objects and allows treating them as a single intersectable entity.
+ *
+ * @author Halimi Lior
+ * @author Nakache Ben
+ */
 public class Geometries extends Intersectable {
+
+    /**
+     * Internal list of all intersectable geometries in the collection.
+     */
     private final List<Intersectable> geometries = new ArrayList<>();
 
+    /**
+     * Constructs a geometry collection and optionally initializes it
+     * with the given intersectable objects.
+     *
+     * @param geometries one or more intersectable geometries to add
+     */
     public Geometries(Intersectable... geometries) {
         add(geometries);
     }
 
+    /**
+     * Adds one or more intersectable geometries to this collection.
+     *
+     * @param geometries one or more intersectable geometries to add
+     */
     public void add(Intersectable... geometries) {
         addAll(this.geometries, geometries);
     }
